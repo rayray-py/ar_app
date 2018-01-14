fe942690ac2aadf591439bc0d721f5d040ef348e
Ray Takahashi <asirei1012@gmail.com>
2018-01-14 17:16:36 +0900

    Import entities reducer

--- src/reducer.root.js
+++ src/reducer.root.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import screensReducer from './screens/reducer.screens';
+import entitiesReducer from './entities/reducer.entities';
 
 export default combineReducers({
   screens: screensReducer,
+  entities: entitiesReducer,
 });
