77b9bd1c99d14b0c5efe4d4e50b5abc1a40ce187
Ray Takahashi <asirei1012@gmail.com>
2018-01-14 17:16:20 +0900

    Initialize firebase

--- src/App.js
+++ src/App.js
@@ -7,10 +7,32 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
+import firebase from 'firebase';
+import { FIREBASE } from './constants';
 import store from './store';
 import Router from './Router';
 
 export default class App extends Component<{}> {
+  componentWillMount() {
+    const {
+      apiKey,
+      authDomain,
+      databaseURL,
+      projectId,
+      storageBucket,
+      messagingSenderId
+    } = FIREBASE;
+
+    firebase.initializeApp({
+      apiKey,
+      authDomain,
+      databaseURL,
+      projectId,
+      storageBucket,
+      messagingSenderId
+    });
+  }
+
   render() {
     return (
       <View style={{ flex: 1 }}>
